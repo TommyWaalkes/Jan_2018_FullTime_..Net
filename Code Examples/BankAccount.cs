5f84d9fba0e2b5f872122373e2c85663cc5c5ad7
Tommy Waalkes <Tommy@grandcircus.co>
2018-01-31 13:37:19 -0500

    Added bank Account TDD

---
+++ Code Examples/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    class BankAccount
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void Deposit(int amount) {
+
+        }
+
+        public static int Withdrawal(int amount)
+        {
+            if (amount <= 1000)
+            {
+                return amount;
+            }
+            else
+            {
+                return -1;
+            }
+            //return amount;
+        }
+
+        public static bool CheckPassword()
+        {
+            return false;
+        }
+
+    }
+}
---
+++ Code Examples/TestBankAccount.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    [TestFixture]
+    class TestBankAccount
+    {
+        //In these tests the Static Assert object will be your BFF
+        [Test]
+        public void WithdrawReturnsMoney()
+        {
+            int expected = 100;
+            int actual = BankAccount.Withdrawal(100);
+            Assert.AreEqual(expected, actual, "Withdrawing $100 Doesn't work");
+
+            expected = 200; 
+            actual = BankAccount.Withdrawal(200);
+            Assert.AreEqual(expected, actual, "Withdrawing $200 Doesn't work");
+
+            expected = -1;
+            actual = BankAccount.Withdrawal(1001);
+            Assert.AreEqual(expected, actual, "You shouldn't be able to withdraw more than 1000, but you just did");
+        }
+
+        [Test]
+        public void DepositPutsMoneyIn()
+        {
+           
+        }
+    }
+}
