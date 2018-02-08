619628cce3f42a3b9833671a3873f43c651f128b
Tommy Waalkes <Tommy@grandcircus.co>
2018-02-08 14:37:20 -0500

    Added FileIO

---
+++ Code Examples/Review/HotDog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTest
+{
+    class HotDog {
+
+        public string Hot { get; set; }
+        public string dog;
+        public int cash;
+        //Get default
+        // return hot;
+
+        //Set default
+        // Hot = input;
+        public HotDog()
+        {
+            dog = "weenie";
+            Hot = "suuuuuper";
+            
+        }
+
+        public string GetMustard()
+        {
+            string n = "mustard";
+            return n;
+        }
+    }
+}
---
+++ Code Examples/Review/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() {"Jakey","Joey","Jimmy", "James"};
+            string name = "tommy";
+            names.Add(name);
+            names.Add("hhhhhhehehehehe");
+            names[0] = "jonathan";
+
+            string[] ns = new string[] {"Wallace","William","Wookles" };
+            ns[0] = "waalkes";
+            
+
+            HotDog d = new HotDog();
+            Console.WriteLine(d.Hot = "hello");
+            Console.WriteLine(d.dog = "Yes this is dog");
+        }
+    }
+}
