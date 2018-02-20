fe649c53841d0101cd076c96bdaaaaa94f5ef24e
Tommy Waalkes <Tommy@grandcircus.co>
2018-02-20 15:18:29 -0500

    Added Lab 20 Resources

---
+++ Code Examples/Lab 20 Resources/Register.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Data.Entity;
+
+namespace Lab20.Models
+{
+    public class Register
+    {
+        [Required]
+        // [DisplayFormat(ConvertEmptyStringToNull = false)]
+        // [StringLength(20, MinimumLength = 2, ErrorMessage = "Enter A Valid First Name")]
+        [Key]
+        public string FirstName { get; set; }
+
+        [Required]
+        // [DisplayFormat(ConvertEmptyStringToNull = false)]
+        // [StringLength(20, MinimumLength =2, ErrorMessage = "Enter A Valid Email")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+
+    public class DBRegisterContext : DbContext
+    {
+        public DbSet<Register> Entries { get; set; }
+
+        public System.Data.Entity.DbSet<Lab20.Models.Item> Items { get; set; }
+    }
+}
